11b277326b1c4050efa3e5b97dfef79325bd7713
Taigen Takeshita <boke0@TaigennoMacBook-Pro.local>
2026-05-15 02:39:37 +0900

    npmrc更新

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -52,5 +52,14 @@ return {
         },
       })
     end
-  }
+  },
+	{
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
+	}
 }
